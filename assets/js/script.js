1677a60f7db33555ecf565447295cbd17e18d208
lukemunsch <lpmunsch@gmail.com>
2021-12-07 14:39:11 +0000

    add content to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,7 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+function getStatus() {
+    
+}
